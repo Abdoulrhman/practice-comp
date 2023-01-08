e84246949e67d6cee67e4206c0a9b88de250b8d1
Abdoulrhman <Abdoulrahman@holo.ae>
2023-01-08 23:03:50 +0200

    add first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Tabs from "./components/Tabs";
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tabs />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Tabs/index.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react'
+import './styles.css'
+
+const Tabs =()=> {
+    const [activeTab, setActiveTab] = React.useState({})
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab)
+    }
+
+    useEffect(() => {
+        console.log('activeTab', activeTab)
+    }, [activeTab])
+
+    
+
+    const tabsArray = [{
+        id: 59886,
+        title: 'Tab1',
+        content: 'Content1'
+    },
+    {
+        id: 65676,
+        title: 'Tab2',
+        content: 'Content2'
+    },
+    {
+        id: 9859,
+        title: 'Tab3',
+        content: 'Content3'
+    
+    }]
+
+  return (
+    <div>
+     <div className='tabs-wrapper'>
+        {tabsArray.map((tab) => {
+            const {id ,title} = tab
+            return (
+                <div className={`tab ${activeTab.id === id ? 'active' : ''}`} onClick={()=>handleTabClick(tab)}> {title}  </div>
+            )
+        })}
+      
+    </div>
+
+    <div className='content-wrapper'>
+        <p>{activeTab.content}</p>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Tabs
